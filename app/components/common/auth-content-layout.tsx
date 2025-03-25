3b5d21db90ac61e0226c1592bf674e866adda63e
Franklin <franklinjaradev@gmail.com>
2025-03-25 03:01:57 -0400

    auth and sidebar

--- app/components/common/auth-content-layout.tsx
+++ app/components/common/auth-content-layout.tsx
@@ -8,15 +8,30 @@ import {
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import {
+  SIDEBAR_COOKIE_NAME,
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { AppSidebar } from "./app-sidebar";
+import { createServerFn } from "@tanstack/react-start";
+import { getCookie } from "vinxi/http";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+// Server function to get a cookie value
+export const getCookieValue = createServerFn({ method: "GET" }).handler(
+  async () => {
+    return getCookie(SIDEBAR_COOKIE_NAME);
+  }
+);
 
 const AuthContentLayout = ({ children }: { children: React.ReactNode }) => {
+  const sidebarStateQuery = useSuspenseQuery({
+    queryKey: ["sidebar_state"],
+    queryFn: () => getCookieValue(),
+  });
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={sidebarStateQuery.data === "true"}>
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b">
